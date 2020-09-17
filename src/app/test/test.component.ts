2b20055c7e2e286740cb8dd54423fb81de2f28b8
jwang173 <jwang173@stevens.edu>
2020-09-18 00:28:39 +0800

    angular

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template:`
+    <h2>
+      Welcome {{name}}
+    <h2>
+    <input [id]="myId" type="text" value="Angular">
+    <input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Angular">
+    <button (click)='buttonClicked($event)'>Greet</button>
+    <p>Button Clicked {{count}} Times </p>
+    {{greeting}}
+    <button (click)="greeting='Welcome boys'">Welcome</button>
+
+    <input #myInput type="text">
+    <button (click)="logMessage(myInput.value)">log</button>
+
+    <input [(ngModel)]="work" type="text">
+            `,
+  // templateUrl: './test.component.html',
+  // styleUrls: ['./test.component.css']
+  styles:[`
+    h2 {
+      color:red;
+    }
+  `]
+})
+export class TestComponent implements OnInit {
+
+  public name = "Stefan";
+  public myId = "testId";
+  public isDisabled = false;
+  public greeting = "";
+  public work = "";
+  count: number=0;
+  /**
+   * buttonClicked
+ : void  */
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public buttonClicked(event): void {
+    this.count ++;
+    console.log(event);
+    this.greeting = event.type;
+  }
+
+  public logMessage(value) {
+    console.log(value);
+  }
+
+}
