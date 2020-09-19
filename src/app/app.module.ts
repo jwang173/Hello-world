9a8ba3354f72fd3dc7811efae73cfefd4b599b11
jwang173 <jwang173@stevens.edu>
2020-09-19 10:07:28 +0800

    clock assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { ClockComponent } from './clock/clock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule,
