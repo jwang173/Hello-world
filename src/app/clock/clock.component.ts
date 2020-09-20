27b760ee97eb51d39dac3d02df302321ce1b1be0
jwang173 <jwang173@stevens.edu>
2020-09-21 06:25:30 +0800

    clock assignment part

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-clock',
-  templateUrl: './clock.component.html',
+  template: `<h1 (click)="output.next('output')">Alert {{type}}</h1>`,
+  // templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.css']
 })
 export class ClockComponent implements OnInit {
 
+  @Input() type: string = "success";
+  @Output() output = new  EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
