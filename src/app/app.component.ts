b78680d09478db63244052c8dcdc6325bac6c86d
jwang173 <jwang173@stevens.edu>
2020-09-21 11:14:06 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,38 @@ import { ClockComponent } from './clock/clock.component';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  template:`<h1 #clockContainer> Container</h1>
-  <button (click)="createComponent()">click it</button>
-  <h2>London Clock</h2>
-  <p>The time now is {{time}}</p>`,
-  entryComponents: [ClockComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // template:`<h1 #clockContainer> Container</h1>
+  // <button (click)="createComponent()">click it</button>
+  // <h2>London Clock</h2>
+  // <p>The time now is {{time}}</p>`,
+  // entryComponents: [ClockComponent],
 })
 export class AppComponent {
   title:string = 'Hello-world';
-  time:string='';
-  @ViewChild('clockContainer', {read: ViewContainerRef}) container;
-  componentRef: ComponentRef<ClockComponent>;
+  // time:string='';
+  // @ViewChild('clockContainer', {read: ViewContainerRef}) container;
+  // componentRef: ComponentRef<ClockComponent>;
 
-  constructor(private resolver: ComponentFactoryResolver) {
+  // constructor(private resolver: ComponentFactoryResolver) {
     
   }
 
-  createComponent() {
+  // createComponent() {
 
-    this.container.clear();
+  //   this.container.clear();
 
-    const factory: ComponentFactory<ClockComponent> = this.resolver.resolveComponentFactory(ClockComponent);
+  //   const factory: ComponentFactory<ClockComponent> = this.resolver.resolveComponentFactory(ClockComponent);
 
-    this.componentRef = this.container.createComponent(factory); // Internally this method will call the create() method from the factory and will append the component as a sibling to our container.
+  //   this.componentRef = this.container.createComponent(factory); // Internally this method will call the create() method from the factory and will append the component as a sibling to our container.
 
-    console.log('componentRef',this.componentRef);
+  //   console.log('componentRef',this.componentRef);
 
-    this.componentRef.instance.type = Math.random() + '';
+  //   this.componentRef.instance.type = Math.random() + '';
 
-    this.time = new Date().toTimeString();
-     
-    console.log('time',this.time);
-  }
-}
+  //   this.time = new Date().toTimeString();
+
+  //   console.log('time',this.time);
+  // }
+// }
